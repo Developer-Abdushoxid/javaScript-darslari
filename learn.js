a0ade17cec6823e8c7c1e2bc27f0db3beeba3760
Developer-Abdushoxid <abdushoxidbannayev@gmail.com>
2024-04-22 18:39:57 +0500

    js darslar 1.2

---
+++ learn.js
@@ -0,0 +1,33 @@
+// const demo = document.getElementById('demo')
+// const button1 = document.getElementById('button1')
+const button2 = document.getElementById('button2')
+
+
+
+// button1.onclick = function myFunction(){
+//  demo.innerHTML = 'Welcome Java Script'
+// }
+
+// button2.onclick = function myFunction(){
+//     demo.innerHTML = 'Good bye'
+//    }
+
+// myFunction();
+
+// document.write(15 + 13)
+
+// alert (5 +9)
+
+// console.log(5 + 55, 87+90)
+
+// button2.onclick = function myfunction(){
+//     window.print()
+// }
+
+const a = 11;
+const b = 85;
+let z = a + b;
+
+console.log(z)
+
+
