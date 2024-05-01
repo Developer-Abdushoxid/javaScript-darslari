c012d0986a52cee7f3b898604f7f42b37c296369
Developer-Abdushoxid <abdushoxidbannayev@gmail.com>
2024-05-01 13:47:03 +0500

    2.2 darslar

--- learn.js
+++ learn.js
@@ -1,6 +1,13 @@
 // const demo = document.getElementById('demo')
 // const button1 = document.getElementById('button1')
-const button2 = document.getElementById('button2')
+// const button2 = document.getElementById('button2')
+// const salom = document.getElementById('salom')
+// const button = document.getElementById('button')
+
+
+// button.onclick = function salom1(){
+//     salom.textContent = 'hello'
+// }
 
 
 
@@ -24,10 +31,157 @@ const button2 = document.getElementById('button2')
 //     window.print()
 // }
 
-const a = 11;
-const b = 85;
-let z = a + b;
+// const a = 11;
+// const b = 85;
+// let z = a + b;
 
-console.log(z)
+// console.log(z)
+
+// let x = 2 + 3 + '5'
+// const demo = document.getElementById('salom')
+
+// demo.innerHTML = x
+
+
+// const cars = ["Saab", "Volvo", "BMW"];
+
+
+// cars[1] = "Toyota";
+
+// cars.push("Audi");
+// cars.push('lacetti')
+// cars[0] = 'gentra'
+// console.log(cars)
+
+/*
+          OPERATORLAR
+
+       Java scriptda operatorlar quyidagi turlarga bo'linadi :
+       
+       1. Arifmetik operatorlar
+       2. Belgilash operatorlari
+       3. Taqqoslash operatorlar
+       4. String operatorlar
+       5. Mantiqiy operatorlar
+       6. Bitwise operatorlar
+       7. Uchlik operatorlar
+       8. Operatorlar turlari
+
+               Arifmetik operatorlar
+
+    1. =  tayinlash operatori
+    2. +  qo'shish operatori
+    3. *  ko'paytirish operatori
+    4. < va >  taqqoshlash operatori
+    5. -  ayirish operatori
+    6. ** darajalash operatori
+    7. / bo'lish operatori
+            
+           Tayinlash operatorlar
+
+    1. =+ qo'shishni tayinlash
+    2. =- ayirishni tayinlash
+    3. =* ko'paytirishni tayinlash va hokazo..
+      
+      Mantiqiy operatorlar
+
+    1. && - logical and  mantiqiy va
+    2. || - logical or  mantiqiy yoki
+    3. ! - logical not  mantiqiy yo'q
+
+        Bitwise tayinlash operatorlari
+
+    1. &=
+    2. ^=
+    3. |= 
+
+       let x = 10
+        x =+ 15
+       let y = 2
+       
+       console.log(x + y)
+       let result = 10 & 12; 
+       console.log(result);
+
+
+               Java scriptda 8 ta ma'lumot turi (data types) mavjud 
+
+        1. string - qator
+        2. number -  raqam
+        3. biginnt
+        4. boolean -  mantiqiy (true, false)
+        5. undefined -  aniqlanmagan
+        6. null 
+        7. symbol -  belgi
+        8. object -  obyekt
+
+        Object ma'lumotlar 3 turga bo'linadi
+
+        1. object
+        2. array
+        3. date
+
+              Obyektlar
+            
+      const name = {} -- bo'sh obyekt
+
+
+
+    //   const person = {
+    //     name: 'ali',
+    //     surname: 'vali',
+    //     phone: 2222222,
+    //     date: 20240402,
+    //     fullname: function(){
+    //      return this.name + ' ' + this.surname
+    //     }
+    //   }
+    //   console.log(person.fullname)
+
+    // const person = {
+    //     firstName: "John",
+    //     lastName : "Doe",
+    //     id  : 5566,
+    //   };
+    //   console.log(person.firstName)
+
+                 HTML holatlari (EVENTS)
+
+    1. onchange - html elementini o'zgartiradi
+    2. onclick - foydalanuvchi html elementini bosganda 
+    3. onmouseover - Foydalanuvchi sichqonchani HTML elementi ustiga siljitadi
+    4. onmouseout - Foydalanuvchi sichqonchani HTML elementidan uzoqlashtiradi
+    5. onkeydown - Foydalanuvchi klaviatura tugmachasini bosadi
+    6. onload - Brauzer sahifani yuklashni tugatdi
+                  
+
+              STRING metodlar
+
+    1. charAt() - satrda belgilangan indeksini qaytaradi
+       let text = 'hello world'
+       console.log(text.charAt(1))
+
+   2. lenght - string uzunligini qaytaradi
+       let text = 'salom'
+       alert(text.length)
+
+   3. charCodeAt() - unicode dagi indeksini qaytaradi
+      let word = 'assalomu aleykum'
+    console.log(word.charCodeAt(1))
+   
+   4. at() - elementning indeksini aniqlaydi
+       let salom = 'hello world'
+       console.log(salom.at(2))
+   
+     */ 
+    
+    
+ 
+
+      
+
+         
+
+
 
 
