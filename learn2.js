22802a6a1ffae00cff6d0b36931702a9414a44d7
Developer-Abdushoxid <abdushoxidbannayev@gmail.com>
2024-06-07 21:10:24 +0500

    new lesson

---
+++ learn2.js
@@ -0,0 +1,84 @@
+                    //    ARRAYS
+
+
+// elementlarni kvadratlash
+
+// let numbers = [1, 2, 3, 4];
+// let squares = numbers.map(function(number) {
+//     return number * number;
+// });
+
+// console.log(squares); // [1, 4, 9, 16]
+
+
+            //  ENG KATTA ELEMENTNI TOPISH
+            
+//  let numbers = [3, 5, 7, 2, 8];
+//  let max = numbers.reduce(function(a, b) {
+    //  return Math.max(a, b);
+//  });
+//  
+//  console.log(max); // 8
+            // 
+
+
+            // REDUCE
+
+//  let numbers = [1, 2, 3, 4, 5];
+//  let sum = numbers.reduce(function(accumulator, currentValue) {
+    //  return accumulator + currentValue;
+//  }, 0);
+//  
+//  console.log(sum); // 15
+        //  
+
+
+        // FILTER
+
+//  let numbers = [1, 2, 3, 4, 5];
+//  let evenNumbers = numbers.filter(function(number) {
+    //  return number % 2 === 0;
+//  });
+//  
+//  console.log(evenNumbers); // [2, 4]
+//  
+
+
+            // MAP
+
+// let numbers = [1, 2, 3];
+// let squaredNumbers = numbers.map(function(number) {
+    // return number * number;
+// });
+// 
+// console.log(squaredNumbers); // [1, 4, 9]
+        //    
+
+
+
+        // FOREACH
+
+//  let fruits = ["apple", "banana", "mango"]
+//  fruits.forEach(function(fruit) {
+    //  console.log(fruit);
+//  });
+//  "apple"
+//  "banana"
+//  "mango"
+//  
+
+
+                //  FOR SIKLI
+
+// let fruits = ["apple", "banana", "mango"]
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+// "apple"
+// "banana"
+// "mango"
+                               
+
+
+
+
