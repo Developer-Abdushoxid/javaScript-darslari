b16eb16f11e4ec9a34db8c63d187fc7b273532f2
Developer-Abdushoxid <abdushoxidbannayev@gmail.com>
2024-08-05 09:21:50 +0500

    filter metod

--- learn2/learn2.js
+++ learn2/learn2.js
@@ -19,11 +19,55 @@
 // console.log(sumRange(10))
 
 
-// bu rekusiya yordamida :
-function sumRange(n) {
-    if (n == 1) {
-        return 1
-    }
-    return n + sumRange(n - 1)
-}
-console.log(sumRange(10))
+// bu rekursiya yordamida :
+// function sumRange(n) {
+//     if (n == 1) {
+//         return 1
+//     }
+//     return n + sumRange(n - 1)
+// }
+// console.log(sumRange(10))
+
+const movies = [
+    { name: 'Avatar 3D', year: 2008, rating: 9},
+    { name: 'Titanik 7D', year: 1997, rating: 7},
+    { name: 'Forsaj 5D', year: 2020, rating: 5},
+    { name: 'Avengers 3D', year: 2012, rating: 3},
+]
+
+// forEach siklida:
+// const newMovies = [];
+// movies.forEach((movie) => {
+//     if( movie.year < 2010){
+//         newMovies.push(movie)
+//     }
+// } )
+// console.log(newMovies)
+
+
+// for siklida :
+// for (let i = 0; i < movies.length; i++){
+//     const movie = movies[i]
+//     if(movie.year < 2010){
+//         newMovies.push(movie)
+//     }
+// }
+// console.log(newMovies)
+
+// filter metodida: 
+
+const filteredMovie = movies.filter((movie) => {
+    return movie.year > 2010
+})
+
+console.log(filteredMovie)
+
+
+
+
+
+
+
+
+
+
